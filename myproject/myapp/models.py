17c487c699b05472683c4d8df2d3ef41ac09d737
NabilibnMahabub <mdnabil760@gmail.com>
2024-08-06 22:00:49 +0600

    new file:   Pipfile 	new file:   Pipfile.lock 	new file:   myproject/db.sqlite3 	new file:   myproject/manage.py 	new file:   myproject/myapp/__init__.py 	new file:   myproject/myapp/__pycache__/__init__.cpython-310.pyc 	new file:   myproject/myapp/__pycache__/admin.cpython-310.pyc 	new file:   myproject/myapp/__pycache__/apps.cpython-310.pyc 	new file:   myproject/myapp/__pycache__/models.cpython-310.pyc 	new file:   myproject/myapp/__pycache__/urls.cpython-310.pyc 	new file:   myproject/myapp/__pycache__/views.cpython-310.pyc 	new file:   myproject/myapp/admin.py 	new file:   myproject/myapp/apps.py 	new file:   myproject/myapp/migrations/0001_initial.py 	new file:   myproject/myapp/migrations/__init__.py 	new file:   myproject/myapp/migrations/__pycache__/0001_initial.cpython-310.pyc 	new file:   myproject/myapp/migrations/__pycache__/__init__.cpython-310.pyc 	new file:   myproject/myapp/models.py 	new file:   myproject/myapp/templates/tours/index.html 	new file:   myproject/myapp/tests.py 	new file:   myproject/myapp/urls.py 	new file:   myproject/myapp/views.py 	new file:   myproject/myproject/__init__.py 	new file:   myproject/myproject/__pycache__/__init__.cpython-310.pyc 	new file:   myproject/myproject/__pycache__/settings.cpython-310.pyc 	new file:   myproject/myproject/__pycache__/urls.cpython-310.pyc 	new file:   myproject/myproject/__pycache__/wsgi.cpython-310.pyc 	new file:   myproject/myproject/asgi.py 	new file:   myproject/myproject/settings.py 	new file:   myproject/myproject/urls.py 	new file:   myproject/myproject/wsgi.py 	new file:   s.txt

---
+++ myproject/myapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Tour(models.Model):
+
+    oregin_country = models.CharField(max_length=64)
+    destiny_country = models.CharField(max_length=64)
+    nights = models.IntegerField()
+    price = models.IntegerField()
+    
+    #str
+    def __str__(self):
+        return (f'Id:{self.id} From {self.oregin_country} To {self.destiny_country} {self.nights} nights costs {self.price}$')
